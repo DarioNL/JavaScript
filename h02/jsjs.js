31b0970e365c4106e703dfadfdc741b0fd711055
HP 8470p <Darioponzo03@gmail.com>
2019-10-15 09:18:20 +0200

    push h02

---
+++ h02/jsjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Mijn geweldige website";
+document.getElementById("subkop").innerText = "Mijn geweldige subkop";
+document.getElementById("aap1").src = "img/Screenshot32.png";
+document.getElementById("aap2").src = "img/Screenshot33.png";
