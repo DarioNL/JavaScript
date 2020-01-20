f30724ad283cf69047aec2db59e7b2c0d60976b5
HP 8470p <Darioponzo03@gmail.com>
2020-01-20 13:10:25 +0100

    push h06

---
+++ h04/Crookapp.js
@@ -0,0 +1,56 @@
+var slideholdermond = document.getElementById("slideholdermond");
+slideholdermond.style.backgroundImage = "url('img/mond1.jpg')";
+var tellermond = 1;
+
+
+slideholdermond.addEventListener("click", function () {
+    slideholdermond.style.backgroundImage = "url('img/mond" + volgendeAfbeeldingmond() + ".jpg')";
+});
+
+function volgendeAfbeeldingmond() {
+    tellermond++;
+    if(tellermond >= 4) {
+        tellermond= 1;
+    }
+    console.log(tellermond);
+    return tellermond;
+}
+
+
+var slideholderneus = document.getElementById("slideholderneus");
+slideholderneus.style.backgroundImage = "url('img/neus1.jpg')";
+var tellerneus = 1;
+
+
+slideholderneus.addEventListener("click", function () {
+    slideholderneus.style.backgroundImage = "url('img/neus" + volgendeAfbeeldingneus() + ".jpg')";
+});
+
+function volgendeAfbeeldingneus() {
+    tellerneus++;
+    if(tellerneus >= 4) {
+        tellerneus= 1;
+    }
+    console.log(tellerneus);
+    return tellerneus;
+}
+
+
+
+var slideholderogen = document.getElementById("slideholderogen");
+slideholderogen.style.backgroundImage = "url('img/oog1.jpg')";
+var tellerogen = 1;
+
+
+slideholderogen.addEventListener("click", function () {
+    slideholderogen.style.backgroundImage = "url('img/oog" + volgendeAfbeeldingogen() + ".jpg')";
+});
+
+function volgendeAfbeeldingogen() {
+    tellerogen++;
+    if(tellerogen >= 4) {
+        tellerogen= 1;
+    }
+    console.log(tellerogen);
+    return tellerogen;
+}
