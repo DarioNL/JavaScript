659264d2062ea0fd06d4eae87e276d7f9f54c961
HP 8470p <Darioponzo03@gmail.com>
2020-01-24 10:31:26 +0100

    push h06

--- h06/memory.js
+++ h06/memory.js
@@ -11,16 +11,30 @@ var selectedpictures = [];
 
 function createHolders() {
     var dogArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
+    var shuffledDogArray = shuffleDogArray(dogArray);
 
     for(var i = 0 ; i < dogArray.length ; i++) {
         console.log("Stapje " + i);
         pictureHolder = document.createElement("div");
         pictureHolder.style.width = "100px";
         pictureHolder.style.height = "100px";
-        pictureHolder.className = "kaarten-holder" + dogArray[i];
-        pictureHolder.id = "kaart" + i;
-        if(i === 6) pictureHolder.innerHTML = "<br>";
+        pictureHolder.className = "kaarten-holder" + shuffledDogArray[i];
+        pictureHolder.id = "kaart" + shuffledDogArray[i];
         kaarten.appendChild(pictureHolder);
         pictureHolder.style.backgroundImage = 'url("img/dog' + dogArray[i] + '.jpg")';
     }
 }
+
+function shuffleDogArray(array) {
+    var j, x, i;
+    for (i = array.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = array[i];
+        array[i] = array[j];
+        array[j] = x;
+    }
+    return array;
+}
+
+
+
